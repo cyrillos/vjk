cdfa724f9e2dfacac28d0e41cffa90406e14f9db
Cyrill Gorcunov <gorcunov@gmail.com>
2018-01-07 21:46:56 +0300

    cli: Split engine class and client
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/client/vjk.py
+++ src/client/vjk.py
@@ -1,117 +1,8 @@
 #!/usr/bin/python3
 
-import argparse
-import datetime
-import logging
-import pprint
 import socket
 import json
-import time
 import sys
-import os
-import re
-
-from datetime import datetime
-
-def today_starts_unix_time():
-    t = time.gmtime()
-    return int(time.mktime((t.tm_year, t.tm_mon, t.tm_mday, 0, 0, 0,
-                            t.tm_wday, t.tm_yday, t.tm_isdst)))
-
-def unix_time():
-    return int(time.time())
-
-def get_loglevel(num):
-    lvl_nums = {
-        4: logging.DEBUG,
-        3: logging.DEBUG,
-        2: logging.INFO,
-        1: logging.WARNING,
-        0: logging.ERROR,
-    }
-    if num in lvl_nums:
-        return lvl_nums[num]
-    return logging.ERROR
-
-parser = argparse.ArgumentParser(prog='vjk.py')
-
-parser.add_argument('--conf', dest = 'conf',
-                    default = 'conf/vjk.json',
-                    help = 'configuration file in JSON format')
-
-sp = parser.add_subparsers(dest = 'cmd')
-for cmd in ['start']:
-    spp = sp.add_parser(cmd, help = 'Start new activity')
-
-for cmd in ['stop']:
-    spp = sp.add_parser(cmd, help = 'Stop current activity')
-
-for cmd in ['list']:
-    spp = sp.add_parser(cmd, help = 'List activities/categories')
-    spp.add_argument('--category', dest = 'category',
-                     help = 'Categories mode', action = 'store_true')
-    spp.add_argument('--from', dest = 'start',
-                     help = 'Time to report from. Format [-|+]number[d|h|m])')
-    spp.add_argument('--to', dest = 'stop',
-                     help = 'Time to report until. Format [-|+]number[d|h|m])')
-
-for cmd in ['add']:
-    spp = sp.add_parser(cmd, help = 'Add activities/categories')
-    spp.add_argument('--category', dest = 'category',
-                     help = 'Categories mode', action = 'store_true')
-    spp.add_argument('--start', dest = 'start',
-                     help = 'Date and time activity started at. \
-                     Format [YYYY-MM-DD] hh:mm:ss.')
-    spp.add_argument('--stop', dest = 'stop',
-                     help = 'Date and time activity stopped at. \
-                     Format [YYYY-MM-DD] hh:mm:ss.')
-
-for cmd in ['edit']:
-    spp = sp.add_parser(cmd, help = 'Edit activity/category')
-    spp.add_argument('--category', dest = 'category',
-                     help = 'Categories mode', action = 'store_true')
-    spp.add_argument('--id', dest = 'id', help = 'Activity/category ID',
-                     required = True)
-
-for cmd in ['delete']:
-    spp = sp.add_parser(cmd, help = 'Delete activity/category')
-    spp.add_argument('--category', dest = 'category',
-                     help = 'Categories mode', action = 'store_true')
-    spp.add_argument('--id', dest = 'id', help = 'Activity/category ID',
-                     required = True)
-
-for cmd in ['exit']:
-    spp = sp.add_parser(cmd, help = 'Stop server')
-
-args, unknown_args = parser.parse_known_args()
-if args.cmd == None:
-    parser.print_help()
-    sys.exit(1)
-
-conf = []
-
-if args.conf != None and os.path.isfile(args.conf):
-    with open(args.conf) as f:
-        conf = json.load(f)
-else:
-    if 'VJKCONF' in os.environ:
-        with open(os.environ['VJKCONF']) as f:
-            conf = json.load(f)
-    else:
-        logging.error("Provide server configuration")
-        sys.exit(1)
-
-if 'loglevel' not in conf:
-    loglevel = logging.DEBUG
-else:
-    loglevel = get_loglevel(conf['loglevel'])
-
-logging.basicConfig(format = '%(asctime)s %(filename)s %(funcName)s %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S', level = loglevel)
-
-logging.debug('Configuration')
-logging.debug(pprint.pformat(conf))
-logging.debug(pprint.pformat(args))
 
 class Vjk:
     def __init__(self, log, conf):
@@ -122,10 +13,14 @@ class Vjk:
         try:
             self.sock.connect((self.addr, int(self.port)))
             self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            self.log.debug("Connected")
+            self.log.debug("Vjk: Connected")
         except:
-            self.log.error("Can't connect")
-            sys.exit(1)
+            self.sock.close()
+            self.sock = None
+            self.log.error("Vjk: Can't connect %s:%s" % (self.addr, self.port))
+
+    def connected(self):
+        return self.sock != None
 
     def receive(self):
         try:
@@ -138,255 +33,105 @@ class Vjk:
     def send(self, obj):
         seq = json.dumps(obj).encode()
         hdr = ("{\"size\":%7d}" % (len(seq))).encode('utf-8')
-        self.log.debug("send: %s %s" % (hdr, seq))
+        self.log.debug("Vjk: send: %s %s" % (hdr, seq))
         self.sock.send(hdr)
         self.sock.send(seq)
         recv = self.receive()
-        self.log.debug("recv: %s" % (repr(recv)))
+        self.log.debug("Vjk: recv: %s" % (repr(recv)))
         return recv
 
-    def activity_start(self, args):
-        self.log.debug("starting %s" % (repr(args)))
-        if len(args) < 1:
-            self.log.error("No arguments passed")
-            return
-        vals = re.split("@|,", args[0])
-        if len(vals) >= 2:
-            data = {
-                'activity': vals[0],
-                'category': vals[1],
-                'time-start': int(time.time())
-            }
-            if len(vals) > 2:
-                data['comment'] = vals[2]
-            obj = { 'cmd': 'activity-start', 'data': data }
-            self.send(obj)
-            return
+    def server_exit(self):
+        self.log.debug("Vjk: server_exit")
+        obj = { 'cmd': 'exit' }
+        return self.send(obj)
 
-    def tsftime(self, timesting):
-        d = datetime.strptime(timesting, '%Y-%m-%d %H:%M:%S')
-        return int(d.timestamp())
-
-    def activity_add(self, start, stop, args):
-        self.log.debug("starting %s" % (repr(args)))
-        if len(args) < 1 or start == None or stop == None:
-            self.log.error("No arguments passed")
-            return
-        vals = re.split("@|,", args[0])
-        if len(vals) >= 2:
-            data = {
-                'time-start': self.tsftime(start),
-                'time-stop': self.tsftime(stop),
-                'activity': vals[0],
-                'category': vals[1],
-            }
-            if len(vals) > 2:
-                data['comment'] = vals[2]
-            obj = { 'cmd': 'activity-add', 'data': data }
-            self.send(obj)
-            return
-
-    def activity_stop(self):
-        self.log.debug("stopping")
-        obj = {
-            'cmd': 'activity-stop',
-            'data': {
-                'time-stop': int(time.time())
-            }
-        }
-        self.send(obj)
-        return
-
-    def activity_edit(self, entry_id, args):
-        self.log.debug("activity editing id %s %s" % (entry_id, repr(args)))
-        if len(args) < 1:
-            self.log.error("No arguments passed")
-            return
-        vals = re.split("@|,", args[0])
-        if len(vals) >= 2:
-            data = {
-                'id': int(entry_id),
-                'activity': vals[0],
-                'category': vals[1],
-            }
-            if len(vals) > 2:
-                data['comment'] = vals[2]
-            if len(vals) > 3:
-                data['time-start'] = vals[3]
-            if len(vals) > 4:
-                data['time-stop'] = vals[4]
-            obj = { 'cmd': 'activity-update', 'data': data }
-            self.send(obj)
-            return
-
-    def category_edit(self, entry_id, args):
-        self.log.debug("category editing id %s %s" % (entry_id, repr(args)))
-        if len(args) < 1:
-            self.log.error("No arguments passed")
-            return
+    def activity_add(self, time_start, time_stop,
+                     activity, category, comment):
+        self.log.debug("Vjk: activity_add: %s %s %s %s %s" %
+                       (repr(time_start), repr(time_stop),
+                       (repr(activity), repr(category), repr(comment))))
+        if time_start == None or activity == None or category == None:
+            return None
         data = {
-            'id': int(entry_id),
-            'category': args[0],
+                'activity': activity,
+                'category': category,
+                'time-start': time_start,
         }
-        obj = { 'cmd': 'category-update', 'data': data }
-        self.send(obj)
-        return
+        if time_stop:
+            data['time-stop'] = time_stop
+        if comment:
+            data['comment'] = comment
+        obj = { 'cmd': 'activity-start', 'data': data }
+        return self.send(obj)
 
-    def exit(self):
-        self.log.debug("exiting")
-        obj = {'cmd': 'exit'}
-        self.send(obj)
-        return
+    def activity_stop(self, time_stop):
+        self.log.debug("Vjk: activity_stop: %s" % (repr(time_stop)))
+        if time_stop == None:
+            return None
+        data = { 'time-stop': time_stop }
+        obj = { 'cmd': 'activity-start', 'data': data }
+        return self.send(obj)
 
-    def dts(self, s):
-        h = s // 3600
-        s -= h * 3600
-        m = s // 60
-        s -= m * 60
-        return (h, m, s)
+    def activity_update(self, eid, time_start, time_stop,
+                        activity, category, comment):
+        self.log.debug("Vjk: activity_update: %s %s %s %s %s %s" %
+                       (repr(eid), repr(time_start), repr(time_stop),
+                       (repr(activity), repr(category), repr(comment))))
+        if eid == None:
+            return None
+        data = { 'id': eid }
+        if activity:
+            data['activity'] = time_start
+        if time_start:
+            data['time-start'] = time_start
+        if time_stop:
+            data['time-stop'] = time_stop
+        if comment:
+            data['comment'] = comment
+        obj = { 'cmd': 'activity-update', 'data': data }
+        return self.send(obj)
 
-    def strftime(self, ts):
-        return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-    def list_report(self, data):
-        catlen = 8
-        namelen = 4
-        for x in data:
-            if len(x['category']) > catlen:
-                catlen = len(x['category'])
-            if len(x['name']) > namelen:
-               namelen = len(x['name'])
-        #
-        # id | name | category | lentgh
-        fmt = "{1:<{0:}}{3:<{2:}}{5:<{4:}}{7:<{6:}}{9:<{8:}}"
-        print(fmt.format(6, 'ID', (namelen + 2), 'Name',
-                         catlen + 2, 'Category',
-                         22, 'Date',
-                         14, 'Duration',
-                         16, 'Comment'))
-        for x in data:
-            if (not 'stop' in x) or x['stop'] == None:
-                x['stop'] = int(time.time())
-                sign = ' *'
-            else:
-                sign = ' '
-            if (not 'comment' in x) or x['comment'] == None:
-                x['comment'] = ''
-            h, m, s = self.dts(int(x['stop']) - int(x['start']))
-            hms = "{0:04d}:{1:02d}:{2:02d}{3:2s}".format(h, m, s, sign)
-            print(fmt.format(6, x['id'], (namelen + 2), x['name'],
-                             catlen + 2, x['category'],
-                             22, self.strftime(int(x['start'])),
-                             14, hms,
-                             16, x['comment']))
-
-    def tf_to_ts(self, ts):
-        if ts != None:
-            m = re.search('([\+\-]?)([0-9]+)([wdhm]?)', ts)
-            if m:
-                if not m.group(2):
-                    return None
-                val = int(m.group(2))
-                if m.group(3) == 'w':
-                    step = 7 * 24 * 60 * 60
-                elif m.group(3) == 'd':
-                    step = 24 * 60 * 60
-                elif m.group(3) == 'h':
-                    step = 60 * 60
-                elif m.group(3) == 'm':
-                    step = 60
-                else:
-                    step = 1
-                current = unix_time()
-                if m.group(1) == '-':
-                    ts = current - val * step
-                else:
-                    ts = current + val * step
-        return ts
-
-    def activity_list(self, start, stop):
-        self.log.debug("listing")
+    def activity_list(self, time_start, time_stop):
+        self.log.debug("Vjk: activity_list: %s %s" %
+                       (repr(time_start), repr(time_stop)))
         data = { }
-        start = self.tf_to_ts(start)
-        if start == None:
-            start = today_starts_unix_time()
-        data['time-start'] = start
-        stop = self.tf_to_ts(stop)
-        if stop != None:
-            data['time-stop'] = stop
-        obj = {'cmd': 'activity-list', 'data': data}
-        recv = self.send(obj)
-        if recv['status'] == 200 and 'data' in recv:
-            self.list_report(recv['data'])
-        return
+        if time_start:
+            data['time-start'] = time_start
+        if time_stop:
+            data['time-sttop'] = time_stop
+        obj = { 'cmd': 'activity-list', 'data': data }
+        return self.send(obj)
 
-    def list_categories(self, data):
-        catlen = 8
-        for x in data:
-            if len(x['category']) > catlen:
-               catlen = len(x['category'])
-        #
-        # id | name | category | lentgh
-        fmt = "{1:<{0:}}{3:<{2:}}"
-        print(fmt.format(6, 'ID', catlen + 2, 'Category'))
-        for x in data:
-            print(fmt.format(6, x['id'], catlen + 2, x['category']))
+    def activity_delete(self, eid):
+        self.log.debug("Vjk: activity_delete: %s" %
+                       (repr(eid)))
+        if eid == None:
+            return None
+        data = { 'id': eid }
+        obj = { 'cmd': 'activity-delete', 'data': data }
+        return self.send(obj)
+
+    def category_add(self, category):
+        if category == None:
+            return None
+        data = { 'category': category }
+        obj = { 'cmd': 'category-add', 'data': data }
+        return self.send(obj)
+
+    def category_update(self, eid, category):
+        if eid == None or category == None:
+            return None
+        data = { 'id': eid, 'category': category }
+        obj = { 'cmd': 'category-update', 'data': data }
+        return self.send(obj)
 
     def category_list(self):
-        self.log.debug("category_list")
-        obj = {'cmd': 'category-list'}
-        recv = self.send(obj)
-        if recv['status'] == 200 and 'data' in recv:
-            self.list_categories(recv['data'])
-        return
+        obj = { 'cmd': 'category-list' }
+        return self.send(obj)
 
-    def category_add(self, args):
-        self.log.debug("category_add")
-        if len(args) < 1:
-            self.log.error("No arguments passed")
-            return
-        obj = {'cmd': 'category-add', 'data': {'category': args[0]}}
-        recv = self.send(obj)
-        return
-
-    def delete(self, entry_id, is_category):
-        self.log.debug("delete")
-        obj = { 'data': { 'id': int(entry_id) }}
-        if is_category:
-            obj['cmd'] = 'category-delete'
-        else:
-            obj['cmd'] = 'activity-delete'
-        self.send(obj)
-        return
-
-vjkcli = Vjk(logging, conf)
-
-if args.cmd == 'start':
-    vjkcli.activity_start(unknown_args)
-
-if args.cmd == 'stop':
-    vjkcli.activity_stop()
-
-if args.cmd == 'exit':
-    vjkcli.exit()
-
-if args.cmd == 'list':
-    if args.category == False:
-        vjkcli.activity_list(args.start, args.stop)
-    else:
-        vjkcli.category_list()
-
-if args.cmd == 'add':
-    if args.category == False:
-        vjkcli.activity_add(args.start, args.stop, unknown_args)
-    else:
-        vjkcli.category_add(unknown_args)
-
-if args.cmd == 'edit':
-    if args.category == False:
-        vjkcli.activity_edit(args.id, unknown_args)
-    else:
-        vjkcli.category_edit(args.id, unknown_args)
-
-if args.cmd == 'delete':
-    vjkcli.delete(args.id, args.category)
+    def category_delete(self, eid):
+        if eid == None:
+            return None
+        data = { 'id': eid }
+        obj = { 'cmd': 'category-delete', 'data': data }
+        return self.send(obj)
